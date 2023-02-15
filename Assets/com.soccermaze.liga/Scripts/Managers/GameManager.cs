c95803a6cf584ba9626217a55d3ccfc28998d4db
SGCEntertainment <nikcxprojects@gmail.com>
2023-02-15 11:13:38 +0300

    update

--- Assets/com.soccermaze.liga/Scripts/Managers/GameManager.cs
+++ Assets/com.soccermaze.liga/Scripts/Managers/GameManager.cs
@@ -3,14 +3,22 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject background;
+
+    [Space(10)]
     [SerializeField] GameObject menu;
     [SerializeField] GameObject game;
     [SerializeField] GameObject leaders;
     [SerializeField] GameObject settings;
 
+    [Space(10)]
+    [SerializeField] GameObject pause;
+    [SerializeField] GameObject win;
+    [SerializeField] GameObject lose;
+
     private void Start()
     {
-        menu.SetActive(true);
+        OpenMenu();
     }
 
     public void OpenLeaders(bool IsOpen)
@@ -23,5 +31,28 @@ public class GameManager : MonoBehaviour
     {
         menu.SetActive(!IsOpen);
         settings.SetActive(IsOpen);
+    }
+
+    public void OpenGame()
+    {
+        background.SetActive(false);
+
+        menu.SetActive(false);
+        game.SetActive(true);
+    }
+
+    public void OpenMenu()
+    {
+        background.SetActive(true);
+
+        game.SetActive(false);
+        pause.SetActive(false);
+
+        menu.SetActive(true);
+    }
+
+    public void SetPause(bool IsPause)
+    {
+        pause.SetActive(IsPause);
     }
 }
